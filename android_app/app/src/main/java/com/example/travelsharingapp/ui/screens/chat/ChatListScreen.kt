c9f0b1dbe0122d7e6387e2671ee4fe89b7a44778
mathieutreves <55842185+mathieutreves@users.noreply.github.com>
2025-06-14 10:55:12 +0200

    added final material

--- android_app/app/src/main/java/com/example/travelsharingapp/ui/screens/chat/ChatListScreen.kt
+++ android_app/app/src/main/java/com/example/travelsharingapp/ui/screens/chat/ChatListScreen.kt
@@ -32,7 +32,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.PrimaryTabRow
-import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
